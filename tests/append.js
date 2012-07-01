8266bbd7a55e49935da7b9cc2e870178cac87c0c
Kev Zettler <kevzettler@gmail.com>
2012-06-30 17:08:21 -0700

    migration bot and version bump

--- tests/append.js
+++ tests/append.js
@@ -1,23 +1,14 @@
 var parasite = require(__dirname+"/../lib/main.js");
+var util = require('util');
 
 var sites = {"www.google.com" : ["/news", "/images", "/finance"]};
 
 var requestGroup = parasite(sites);
 
-requestGroup.addListener("each", function(response){
-   console.log("individual responses", response);
+requestGroup.on("response", function(response){
+   console.log("individual responses", response.body);
 });
 
-
-requestGroup.append({
-  "www.yahoo.com" : ["/news", "/games", "/autos"]
-});
-
-requestGroup.addListener("end", function(responseTree){
-   console.log("omg response tree", responseTree);
-});
-
-
-requestGroup.append({
-  "www.aol.com" : ["/news", "/games", "/video"]
+requestGroup.on("end", function(responseTree){
+   console.log("omg response tree", util.inspect(responseTree));
 });
